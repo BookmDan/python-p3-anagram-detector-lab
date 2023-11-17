62db7056eafe1c0b6332521f4ce2cf1a47f90152
BookmDan <danoh329@gmail.com>
2023-11-17 14:19:14 -0800

    pass

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word = word 
+
+  def match(self, word_list):
+    matching = []
+    for word in word_list:
+      if self.is_anagram(word):
+        matching.append(word)
+    return matching
+
+  def is_anagram(self, other_word):
+    return sorted(self.word.lower()) == sorted(other_word.lower())
